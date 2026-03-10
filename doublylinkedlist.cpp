b33a0b1f540486abb576639be98edf83fd1395c6
Karleigh Branscomb <kbranscomb@ualr.edu>
2026-03-10 01:16:57 +0000

    edited doubly linked list cpp; added comments

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -71,15 +71,18 @@ void DoublyLinkedList::displayDoublyLinkedList()
 
 void DoublyLinkedList::drawDoublyLinkedList()
 {
- 
+ //node is assigned at head
   Node* tempNode = head;
 
+  //loop as long as the temporary node is not NULL
   while(tempNode != NULL){
+	//print out addresss, previous value, value, and next value
 	cout << "[address:" << tempNode 
 	<< ", prev:" << tempNode->prev 
 	<< ", value: " << tempNode->value 
 	<< ", next: " << tempNode->next << "]\n";
 
+	//go to next value and assign tempoNode that value
 	tempNode = tempNode->next;
   }
 
@@ -89,17 +92,19 @@ void DoublyLinkedList::drawDoublyLinkedList()
 bool DoublyLinkedList::isPalindrome()
 {
 
+	//assign headnode to head and tailnode to tail.
 	Node* headnode = head;
 	Node* tailnode = tail;
 
-	
+	//if headnode and tailnode is both NULL return TRUE
 	if(headnode == NULL && tailnode == NULL){
 		return true;
 	}
 
+	//as long as headnode and tailnode don't end up at the same spot and cross eachother go to next spot
 	while(headnode != tailnode && headnode != tailnode->next){
 		
-
+		//if the head and tail don't equal the same value return false
 		if(headnode->value != tailnode->value){
 
 			return false;
@@ -110,13 +115,14 @@ bool DoublyLinkedList::isPalindrome()
 		
 
 	}
-
+	//return to true after while loop finishes
 	return true;
 
 }
 
 void DoublyLinkedList::split(int n) // n is the number of even partitions
 {	
+	//assign headnode to the head
  	Node* headnode = head;
 
 	//checks size of list
@@ -126,13 +132,15 @@ void DoublyLinkedList::split(int n) // n is the number of even partitions
 		size++;
 		headnode = headnode->next;
 	}
+
+	//go back to the start of the list
 	headnode = head;	
 
 
-	//divides the size of the list 
+	//divide the size of list by n to get even amount
 	int div = size/n;
 
-
+	//if n is less than, greater than the sze of the list, and if the size is not even, print: Cannot be Processed
 	if(n<1 || n>size || size % n !=0 ){
 
 		std::cout << "Cannot be processed\n";
@@ -141,8 +149,10 @@ void DoublyLinkedList::split(int n) // n is the number of even partitions
 	}
 	
 
+	//loop n times of partitions
 	for(int i = 0; i < n; i++){
 
+		//go through the list and print the even amount of numbers
 		for(int j=0; j<div; j++){
 
 			std::cout << headnode->value;
@@ -159,21 +169,31 @@ void DoublyLinkedList::split(int n) // n is the number of even partitions
 }
 
 void DoublyLinkedList::addNewNodeBeforeTargetNode(Node* newNode, int val){
-Node *target = head; 
-	Node *prevNode = target;
-	while(target != NULL){
+	//assign a node for the targetnode and assign it to heaad
+	Node *targetNode = head; 
 
-		
-		if(target->value == val){
-			target->prev = newNode;
+	//making prevNode later...
+	Node *prevNode;
+
+	//as long as tartget node isn't NULL, keep going
+	while(targetNode != NULL){
+
+		//If we fid the matching value do this: 
+		if(targetNode->value == val){
+			//assign the value behind the target to the new node
+			targetNode->prev = newNode;
+			//assign the value in front of the previous node (the node that's following behind the target)
             prevNode->next = newNode;
+			//assign the value behind the newnode the value that was previously there
             newNode->prev = prevNode;
-            newNode->next = target;
+			//assign the value in front of the newnode the target node.
+            newNode->next = targetNode;
 			break;
 		}
 
-		prevNode = target;
-		target = target->next;
+		//prevNode will follow behind target node after each loop
+		prevNode = targetNode;
+		targetNode = targetNode->next;
   
 }
 
